e98dd2f48e336eff08524624335a5034415e47f2
LuisRomero76 <romero761105@gmail.com>
2024-05-26 09:53:35 -0400

    Tema 5

--- Ejercicios en Clases/Tema 5/Combos dependientes/provincias.php
+++ Ejercicios en Clases/Tema 5/Combos dependientes/provincias.php
@@ -1,7 +1,12 @@
 <?php
 include('conexion.php');
+$iddepartamento = $_GET['id'];
 
-$sql = "SELECT id, provincia, iddepartamento FROM provincias";
+$sql = "SELECT p.id, p.provincia, p.iddepartamento 
+        FROM provincias p JOIN  departamentos d 
+        ON p.iddepartamento = d.id
+        WHERE d.id =".$iddepartamento;
+
 $resultado = $con->query($sql);
 $datos = array();
 if ($resultado->num_rows > 0) {
